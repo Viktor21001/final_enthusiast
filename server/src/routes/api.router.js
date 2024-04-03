f787f939af4d6098f5d5b33c044ff74f3e4ed80f
Dali <khashievadali@gmail.com>
2024-04-04 00:00:09 +0300

    newMemberBack

--- server/src/routes/api.router.js
+++ server/src/routes/api.router.js
@@ -3,12 +3,12 @@ const apiRouter = require('express').Router();
 const ideaRouter = require('./idea.router');
 const startupRouter = require('./startup.router');
 const favoriteRouter = require('./favorites.router');
-// const themeRouter = require('./theme.router');
 const userRouter = require('./user.router');
+const memberRouter = require('./members.router');
 
 module.exports = apiRouter
   .use('/users', userRouter)
   .use('/ideas', ideaRouter)
   .use('/startups', startupRouter)
-  .use('/favorites', favoriteRouter);
-// .use('/themes', themeRouter);
+  .use('/favorites', favoriteRouter)
+  .use('/members', memberRouter);
